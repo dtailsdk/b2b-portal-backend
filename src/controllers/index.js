ad1f7edb4f66f5358b013bb5e18d1257a2064b0e
Louise Hedegaard <louise@dtails.dk>
2023-01-17 23:12:20 +0100

    Simple app proxy example

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 import { Server } from '@mekanisme/server'
 
+import b2bPortalRouter from './b2b-portal-controller'
 import appsRouter from './apps-controller'
 import shopRouter from './shops-controller'
 import webhooksRouter from './webhooks-controller'
@@ -7,6 +8,7 @@ import webhooksRouter from './webhooks-controller'
 import { validateAllWebhooks } from '../lib/webhook-service'
 
 export default function init(shopifyOAuth) {
+  Server.use('/b2b-portal', b2bPortalRouter(shopifyOAuth))
   Server.use('/app', appsRouter(shopifyOAuth))
   Server.use('/app/shops', shopRouter(shopifyOAuth))
   Server.use('/app/webhooks', webhooksRouter(shopifyOAuth))
