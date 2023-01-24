d5bbe2ec81a246786b224249494df58189e9eba9
Louise Hedegaard <louise@dtails.dk>
2023-01-24 13:14:07 +0100

    Configuration file is specified in env variable

--- src/lib/configuration-service.js
+++ src/lib/configuration-service.js
@@ -1,14 +1,16 @@
 import { ShopifyToken } from 'models'
-import configurations from '../../configurations.json'
+import { getEnvironment } from '@dtails/toolbox/lib'
+import fs from 'fs-extra'
 
 const VALID_CHECKOUT_TYPES = ['B2B_PORTAL', 'SHOPIFY']
+let configurations = null
 
 export async function validateAllConfigurations() {
   const shops = await ShopifyToken.q.whereNull('uninstalledAt').withGraphFetched('app')
   console.log(`Found ${shops.length} shops to validate`)
+  const allConfigurations = await getConfigurations()
   for (const shop of shops) {
-    console.log(`Going to validate configuration for shop ${shop.shop} with identifier ${shop.app.identifier}`)
-    await validateConfiguration(configurations[shop.app.identifier])
+    await validateConfiguration(allConfigurations[shop.app.identifier])
     console.log(`Successfully validated configuration for shop ${shop.shop} with identifier ${shop.app.identifier}`)
   }
 }
@@ -21,4 +23,15 @@ export async function validateConfiguration(configuration) {
     throw Error(`configuration.checkoutConfiguration must be one of [${VALID_CHECKOUT_TYPES}] but was ${configuration.checkoutConfiguration.type}`)
   }
   //TODO Validate remaining parameters when they have been defined
+}
+
+export async function getConfigurations() {
+  if (!configurations) {
+    console.log('Lazy loading configurations')
+    const fileName = getEnvironment('CONFIGURATIONS_FILE_NAME')
+    const filePath = `${__dirname}/../../${fileName}`
+    const fileContent = await fs.readFile(filePath, { encoding: 'utf8' })
+    configurations = JSON.parse(fileContent)
+  }
+  return configurations
 }
