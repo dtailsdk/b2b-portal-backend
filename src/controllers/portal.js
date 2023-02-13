d0f7310385b8fb3da8c8549cfc6ff3c8e6903d1d
Mickey Binder <mickey@dtails.dk>
2023-02-13 09:11:34 +0100

    add shipping and order methods

--- src/controllers/portal.js
+++ src/controllers/portal.js
@@ -5,7 +5,9 @@ import { getEnvironment } from '@dtails/toolbox-backend'
 import querystring from 'querystring'
 import jwt from 'jsonwebtoken'
 import { computeSignature } from '../lib/security-service'
-
+import { getStoreByName } from '../lib/shop-service'
+import { convertToDraftOrder, getShippingForOrder, createOrder } from '../lib/order-service'
+import { delay } from '@dtails/toolbox-backend'
 /**
  * Controller for the calls from the B2B portal
  */
@@ -15,28 +17,58 @@ import { computeSignature } from '../lib/security-service'
  */
 async function getToken(req, res) {
   try {
-    const dbShopName = req.headers['x-shop-domain'].replace('.myshopify.com', '')
+    const storeName = req.headers['x-shop-domain']
+    const dbShopName = storeName.replace('.myshopify.com', '')
     const shop = await ShopifyToken.query().withGraphJoined('app').findOne({ shop: dbShopName })
     const appSecret = shop.app.shopifyAppSecret
-
     const originalQuerystring = req.url.substring(req.url.indexOf('?') + 1)
     const signatureFromClient = querystring.parse(originalQuerystring).signature
     const computedSignature = computeSignature(originalQuerystring, appSecret)
-
     if (signatureFromClient !== computedSignature || !req.query.logged_in_customer_id) {
-      return res.status(401)
+      return res.sendStatus(401)
     }
-
     const signingKey = getEnvironment('SIGN_KEY')
     var token = jwt.sign({
-      userId: req.query.logged_in_customer_id
+      storeName,
+      customerId: req.query.logged_in_customer_id
     }, signingKey)
     return res.send({ token })
   } catch (error) {
     log(error.message)
     trace(error)
+    return res.send('ERROR')
   }
+}
 
+async function getCustomer(req, res) {
+  const store = await getStoreByName(req.storeName)
+  const customer = await store.api().customer.getById(req.customerId)
+  return res.send(customer)
+}
+
+async function getShipping(req, res) {
+  //input: address, cart
+  const { cart, address} = req.body
+  const store = await getStoreByName(req.storeName)
+  const customerId = req.customerId
+  const customer = await store.api().customer.getById(customerId)
+  const draftOrder = convertToDraftOrder(customerId, customer.email, cart, address)
+  const shippingMethod = await getShippingForOrder(store.api(), draftOrder)
+  
+  res.send(shippingMethod)
+}
+
+async function createOrderFromCart(req, res) {
+  //input: address, cart
+  const { cart, address} = req.body
+  const store = await getStoreByName(req.storeName)
+  const customerId = req.customerId
+  const customer = await store.api().customer.getById(customerId)
+  const draftOrder = convertToDraftOrder(customerId, customer.email, cart, address)
+  const order = await createOrder(store.api(), draftOrder)
+  
+  await delay(5000) //Wait 5 seconds, to be sure the order has been created locally
+  return res.json(order)
 }
 
 async function getProducts(req, res) {
@@ -57,7 +89,14 @@ async function authenticateToken(req, res, next) {
   if (token == null) return res.sendStatus(401)
 
   jwt.verify(token, signingKey, async (err, payload) => {
-    if (err) return res.sendStatus(403)
+    if (err) {
+      console.log('error verifying token', err)
+      return res.sendStatus(403)
+    }
+    console.log('TOKEN', payload)
+    req.customerId = payload.customerId
+    req.storeName = payload.storeName
+    
     next()
   })
 }
@@ -66,19 +105,38 @@ export default function init() {
   const router = Server.Router()
 
   router
-    .route('/tokens')
+    .route('/token')
     .get(getToken)
-    .all(Server.middleware.methodNotAllowed)
-
-  router
-    .route('/products')
-    .get(authenticateToken, getProducts)
     .all(Server.middleware.methodNotAllowed)
 
   router
     .route('/ping')
     .get(ping)
     .all(Server.middleware.methodNotAllowed)
+
+  /*router
+    .route('*')
+    .all(authenticateToken)*/
+
+  router
+    .route('/get_customer')
+    .get(authenticateToken, getCustomer)
+    .all(Server.middleware.methodNotAllowed)
+
+  router
+    .route('/get_shipping')
+    .post(authenticateToken, getShipping)
+    .all(Server.middleware.methodNotAllowed)
+
+  router
+    .route('/create_order')
+    .post(authenticateToken, createOrderFromCart)
+    .all(Server.middleware.methodNotAllowed)
+
+  router
+    .route('/products')
+    .get(authenticateToken, getProducts)
+    .all(Server.middleware.methodNotAllowed)  
 
   return router
 }
